cfc6bcda722738de13f6d19f7c07feaa9d089be5
azizurrbappy <azizurr.bappy@gmail.com>
2025-11-23 19:38:45 +0600

    eDex project setup complete

---
+++ src/app/layout.jsx
@@ -0,0 +1,27 @@
+import { Public_Sans } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/app/Shared/Navbar';
+
+const publicSans = Public_Sans({
+  variable: '--font-public-sans',
+  subsets: ['latin'],
+});
+
+export const metadata = {
+  title: 'eDex - Online Course E-Learning Website',
+  description: 'Online Course E-Learning Website',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${publicSans.variable} antialiased flex flex-col min-h-screen`}
+      >
+        <Navbar></Navbar>
+        <section className="flex-1"></section>
+        Footer
+      </body>
+    </html>
+  );
+}
