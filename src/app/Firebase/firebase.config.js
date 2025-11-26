20396deb5a8621a494106d0b07e11eaa089ccfe3
azizurrbappy <azizurr.bappy@gmail.com>
2025-11-26 17:53:54 +0600

    fix en problem

--- src/app/Firebase/firebase.config.js
+++ src/app/Firebase/firebase.config.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAV4VzWf2VAWpR42lQDALdBQ0ITSIj8-y4',
-  authDomain: 'edex-9813b.firebaseapp.com',
-  projectId: 'edex-9813b',
-  storageBucket: 'edex-9813b.firebasestorage.app',
-  messagingSenderId: '937038121992',
-  appId: '1:937038121992:web:a4436230345145151ed5bd',
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
